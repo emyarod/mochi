c2d42e7ad6a1f9fa27637fec15f52e2097937a3f
emyarod <afw5059@gmail.com>
2020-05-23 18:45:05 -0500

    prevent reloads and repeated videos

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const N = 7;
-const randN = n => Math.floor(Math.random() * n);
+const shuffle = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+const indices = shuffle([...Array(8)].map((_, i) => i));
+let index = indices.indexOf(7);
 const video = document.querySelector('#mochi');
-video.src = `public/${randN(N)}.webm`;
-video.addEventListener('click', () => window.location.reload());
+video.addEventListener('click', () => {
+  index = index + 1 === indices.length ? 0 : index + 1;
+  video.src = `public/${index}.webm`;
+});
